47eb3083d5078c044af9a97b5c3603f59d8b516c
Niyonkuru-olivier <oniyonkuru44@gmail.com>
2026-05-06 23:23:13 +0200

    chore: ignore build artifacts and remove dist from tracking

--- backend/dist/mail/mail.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class MailService {
-    private configService;
-    private transporter;
-    private readonly logger;
-    constructor(configService: ConfigService);
-    sendWelcomeEmail(to: string, name: string, tempPassword: string): Promise<void>;
-    sendResetPasswordEmail(to: string, name: string, resetToken: string): Promise<void>;
-}
